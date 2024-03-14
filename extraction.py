0709b6fad377ec3470aca34dd3d58d6f0edac6d1
Thomas Wang <thomaswang@berkeley.edu>
2024-03-13 20:36:48 -0700

    updated skeleton

--- extraction.py
+++
@@ -1,4 +0,0 @@
-#!./venv/bin/python3.10
-
-if __name__ == "__main__":
-    raise NotImplementedError("This module has not been implemented yet.")
---
+++ extraction/extraction.py
@@ -0,0 +1,10 @@
+#!./venv/bin/python3.10
+""" Main script for extracting data from check images. 
+
+This module is responsible for orchestrating the different extraction methods 
+in the extraction package.
+"""
+
+
+if __name__ == "__main__":
+    raise NotImplementedError("This module has not been implemented yet.")

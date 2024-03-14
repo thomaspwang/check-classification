f76de2824d98982863a0d376ffe0931dfcdc9cf5
Thomas Wang <thomaswang@berkeley.edu>
2024-03-13 21:46:09 -0700

    Added more skeleton for extract_handwriting.py

--- extraction/extract_handwriting.py
+++ extraction/extract_handwriting.py
@@ -15,4 +15,46 @@ def parse_handwriting(
         box : BoundingBox,
         mode : Mode,
 ) -> str:
+    match mode:
+        case Mode.DOC_TR:
+            return parse_handwriting_doctr(img_path, region, box)
+        case Mode.TR_OCR:
+            return parse_handwriting_trocr(img_path, region, box)
+        case Mode.LLAVA:
+            return parse_handwriting_llava(img_path, region, box)
+        case Mode.AMAZON_TEXTRACT:
+            return parse_handwriting_amazon_textract(img_path, region, box)
+        case _:
+            raise ValueError(f"Invalid mode: {mode}")
+
+def parse_handwriting_doctr(
+        img_path: Path,
+        region : RegionType,
+        box : BoundingBox,
+) -> str:
+    """ Parse handwriting using DocTR model """
+    raise NotImplementedError("This function has not been implemented yet.")
+
+def parse_handwriting_trocr(
+        img_path: Path,
+        region : RegionType,
+        box : BoundingBox,
+) -> str:
+    """ Parse handwriting using TrOCR model """
+    raise NotImplementedError("This function has not been implemented yet.")
+
+def parse_handwriting_llava(
+        img_path: Path,
+        region : RegionType,
+        box : BoundingBox,
+) -> str:
+    """ Parse handwriting using LLAVA model """
+    raise NotImplementedError("This function has not been implemented yet.")
+
+def parse_handwriting_amazon_textract(
+        img_path: Path,
+        region : RegionType,
+        box : BoundingBox,
+) -> str:
+    """ Parse handwriting using Amazon Textract """
     raise NotImplementedError("This function has not been implemented yet.")

d49b158b9b14544e4d3e4624d72f09643fc2b1ed
EC2 Default User <ec2-user@ip-172-16-78-208.us-east-2.compute.internal>
2024-05-15 09:00:44 +0000

    added doctr and llava

--- extraction/extract_handwriting.py
+++ extraction/extract_handwriting.py
@@ -2,12 +2,32 @@ from enum import Enum
 from pathlib import Path
 from extract_bboxes import BoundingBox
 
+from doctr.io import DocumentFile
+from doctr.models import ocr_predictor
+from PIL import Image
+from llava_wrapper import LLaVA
+
 import sys
+
+TEMPFILE_PATH = "/tmp/tempfile.jpg"
+MODEL = ocr_predictor(pretrained=True)
+LLAVA = LLaVA("liuhaotian/llava-v1.6-34b")
+PROMPT = "print the text in the image."
 
 class Mode(Enum):
     DOC_TR = "DocTR"
     LLAVA = "LLAVA"
     # add more as needed
+
+def crop_image(image_path, bounding_box):
+    image = Image.open(image_path)
+    if image.mode == 'RGBA':
+        image = image.convert('RGB')
+    x1, y1 = bounding_box.top_left_corner()
+    x2, y2 = bounding_box.bottom_right_corner()
+    cropped_image = image.crop((x1, y1, x2, y2))
+    cropped_image.save(TEMPFILE_PATH)
+
 
 def parse_handwriting(
         img_path: Path,
@@ -28,23 +48,33 @@ def parse_handwriting_doctr(
         box : BoundingBox,
 ) -> str:
     """ Parse handwriting using DocTR model """
-    raise NotImplementedError("This function has not been implemented yet.")
+    crop_image(img_path, box)
+    doc = DocumentFile.from_images(TEMPFILE_PATH)
+    result = MODEL(doc)
+    concatString = ""
+    for block in result.pages[0].blocks:
+        for line in block.lines:
+            for word in line.words:
+                concatString = concatString + word.value + " "
+    return concatString[:-1]
+
 
 def parse_handwriting_llava(
         img_path: Path,
         box : BoundingBox,
 ) -> str:
     """ Parse handwriting using LLAVA model """
-    raise NotImplementedError("This function has not been implemented yet.")
+    crop_image(img_path, box)
+    return model.eval(TEMPFILE_PATH,PROMPT)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 8:
-        print("Usage: python3 extract_handwriting.py <image_path> <box_min_x> <box_min_y> <box_max_x> <box_max_y> <mode>")
+    if len(sys.argv) != 7:
+        print("Usage: python3 extract_handwriting.py <image_path> <x> <y> <width> <height> <mode>")
         sys.exit(1)
     
     # Parse the image path from command-line arguments
     image_path = Path(sys.argv[1])
-    boundingBox = BoundingBox(min_x=int(sys.argv[2]), min_y=int(sys.argv[3]), max_x=int(sys.argv[4]), max_y=int(sys.argv[5]))
+    boundingBox = BoundingBox(x=int(sys.argv[2]), y=int(sys.argv[3]), width=int(sys.argv[4]), height=int(sys.argv[5]))
     mode = Mode(sys.argv[6])
     
     parsed_string = parse_handwriting(image_path, boundingBox, mode)
---
+++ extraction/llava_wrapper.py
@@ -0,0 +1,125 @@
+import argparse
+import torch
+
+from llava.constants import (
+    IMAGE_TOKEN_INDEX,
+    DEFAULT_IMAGE_TOKEN,
+    DEFAULT_IM_START_TOKEN,
+    DEFAULT_IM_END_TOKEN,
+    IMAGE_PLACEHOLDER,
+)
+from llava.conversation import conv_templates, SeparatorStyle
+from llava.model.builder import load_pretrained_model
+from llava.utils import disable_torch_init
+from llava.mm_utils import (
+    process_images,
+    tokenizer_image_token,
+    get_model_name_from_path,
+)
+
+import requests
+from PIL import Image
+from io import BytesIO
+import re
+
+class LLaVA:
+    # TODO: make model_path an enum
+    def __init__(self, model_path, model_base=None, conv_mode=None, temperature=0, top_p=None, num_beams=1, max_new_tokens=512):
+        disable_torch_init()
+        self.conv_mode = conv_mode
+        self.temperature = temperature
+        self.top_p = top_p
+        self.num_beams = num_beams
+        self.max_new_tokens = max_new_tokens
+
+        self.model_name = get_model_name_from_path(model_path)
+        self.tokenizer, self.model, self.image_processor, self.context_len = load_pretrained_model(
+            model_path, model_base, self.model_name
+        )
+
+    def load_image(self, image_file):
+        if image_file.startswith("http") or image_file.startswith("https"):
+            response = requests.get(image_file)
+            image = Image.open(BytesIO(response.content)).convert("RGB")
+        else:
+            image = Image.open(image_file).convert("RGB")
+        return image
+
+    def process_query(self, query, args_conv_mode, model_name):
+        qs = query
+        image_token_se = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN
+        if IMAGE_PLACEHOLDER in qs:
+            if True or self.model.config.mm_use_im_start_end: # i think this is for first msg
+                qs = re.sub(IMAGE_PLACEHOLDER, image_token_se, qs)
+            else:
+                qs = re.sub(IMAGE_PLACEHOLDER, DEFAULT_IMAGE_TOKEN, qs)
+        else:
+            if True or self.model.config.mm_use_im_start_end: # i think this is for first msg
+                qs = image_token_se + "\n" + qs
+            else:
+                qs = DEFAULT_IMAGE_TOKEN + "\n" + qs
+
+        if "llama-2" in self.model_name.lower():
+            conv_mode = "llava_llama_2"
+        elif "mistral" in self.model_name.lower():
+            conv_mode = "mistral_instruct"
+        elif "v1.6-34b" in self.model_name.lower():
+            conv_mode = "chatml_direct"
+        elif "v1" in self.model_name.lower():
+            conv_mode = "llava_v1"
+        elif "mpt" in self.model_name.lower():
+            conv_mode = "mpt"
+        else:
+            conv_mode = "llava_v0"
+
+        if args_conv_mode is not None and conv_mode != args_conv_mode:
+            raise(Exception("This exception should not be thrown"))
+            print(
+                "[WARNING] the auto inferred conversation mode is {}, while `--conv-mode` is {}, using {}".format(
+                    conv_mode, args_conv_mode, args_conv_mode
+                )
+            )
+        else:
+            args_conv_mode = conv_mode
+
+        conv = conv_templates[args_conv_mode].copy()
+        conv.append_message(conv.roles[0], qs)
+        conv.append_message(conv.roles[1], None)
+        return conv.get_prompt()
+
+
+    def eval(self, image_file, query):
+        prompt = self.process_query(query, self.conv_mode, self.model_name)
+        images = [self.load_image(image_file)]
+        image_sizes = [x.size for x in images]
+        images_tensor = self.process_images(
+            images,
+            self.image_processor,
+            self.model.config
+        ).to(self.model.device, dtype=torch.float16)
+
+        input_ids = (
+            tokenizer_image_token(prompt, self.tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt")
+            .unsqueeze(0)
+            .cuda()
+        )
+
+        with torch.inference_mode():
+            output_ids = model.generate(
+                input_ids,
+                images=images_tensor,
+                image_sizes=image_sizes,
+                do_sample=True if self.temperature > 0 else False,
+                temperature=self.temperature,
+                top_p=self.top_p,
+                num_beams=self.num_beams,
+                max_new_tokens=self.max_new_tokens,
+                use_cache=True,
+            )
+
+        outputs = self.tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0].strip()
+        return outputs
+
+#model = LLaVA("liuhaotian/llava-v1.6-34b")
+#PROMPT = "Scan the check and list only the following information in key:value form separated by newlines: Check Number, Check Amount, Payer First Name, #Payer Last Name, Payer Routing Number, Payer Account Number."
+#model.eval( "../dataset3/mcd-test-3-front-images/mcd-test-3-front-3.jpg",PROMPT)
